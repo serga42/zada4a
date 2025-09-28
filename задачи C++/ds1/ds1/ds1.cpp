a9271643c4ede5f6147b2b45c55da75d8542ab09
serga42 <seregaprotaso@yandex.ru>
2025-09-28 18:29:03 +0300

    1

---
+++ задачи C++/ds1/ds1/ds1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+    int n;
+
+    cout << "size mertick (n): ";
+    cin >> n;
+
+    int** matrix = new int* [n];
+    for (int i = 0; i < n; i++) {
+        matrix[i] = new int[n];
+    }
+
+    // Заполнение матрицы
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            if (i == j) {
+                // Главная диагональ: числа от n до 1
+                matrix[i][j] = n - i;
+            }
+            else {
+                // Остальные элементы - случайные числа от 0 до 9
+                matrix[i][j] = rand() % 10;
+            }
+        }
+    }
+
+    cout << "\nРезультирующая матрица:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << setw(3) << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    // Освобождение памяти
+    for (int i = 0; i < n; i++) {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+
+    return 0;
+}
